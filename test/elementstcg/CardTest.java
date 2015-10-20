ba21392814cd6d7014ed7ef134351bc59156e6fe
Locitao <rvdtera@gmail.com>
2015-10-20 20:58:33 +0200

    Adding javadoc

--- test/elementstcg/CardTest.java
+++ test/elementstcg/CardTest.java
@@ -1,6 +1,7 @@
 package elementstcg;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,8 +14,17 @@ import static org.junit.Assert.assertTrue;
  */
 public class CardTest extends TestCase {
 
-    private Card card = new Card(Element.Fire, 10, 15, "testcard", 4);
+    private Card card;
 
+    @Before
+    public void setUp() {
+        card = new Card(Element.Fire, 10, 15, "testcard", 4);
+    }
+
+    /**
+     * Tests for the getHp() method.
+     * @throws Exception
+     */
     @Test
     public void testGetHP() throws Exception {
         assertNotNull("Card is null", card);
@@ -23,17 +33,29 @@ public class CardTest extends TestCase {
         assertNotNull("HP can't be null", card.getHP());
     }
 
+    /**
+     * Tests for the getName() method;
+     * @throws Exception
+     */
     @Test
     public void testGetName() throws Exception {
         assertEquals("Name isn't equal", "testcard", card.getName());
         assertNotNull("Name can't be null", card.getName());
     }
 
+    /**
+     * Tests for the getAttacked() method.
+     * @throws Exception
+     */
     @Test
     public void testGetAttacked() throws Exception {
         assertFalse("Attacked isn't false", card.getAttacked());
     }
 
+    /**
+     * Tests for the setAttacked method.
+     * @throws Exception
+     */
     @Test
     public void testSetAttacked() throws Exception {
         card.setAttacked(true);
@@ -41,18 +63,30 @@ public class CardTest extends TestCase {
         card.setAttacked(false);
     }
 
+    /**
+     * Tests the method getElement();
+     * @throws Exception
+     */
     @Test
     public void testGetElement() throws Exception {
         assertEquals("Element isn't correct", Element.Fire, card.getElement());
         assertNotNull("Element can't be null", card.getElement());
     }
 
+    /**
+     * Test the getCapacityPoints() method.
+     * @throws Exception
+     */
     @Test
     public void testGetCapacityPoints() throws Exception {
         assertEquals("CP isn't correct", 4, card.getCapacityPoints());
         assertNotNull("CP can't be null", card.getCapacityPoints());
     }
 
+    /**
+     * Tests the modifyHp() method.
+     * @throws Exception
+     */
     @Test
     public void testModifyHP() throws Exception {
         Card healthCard = new Card(Element.Fire, 10, 15, "testcard", 4);
@@ -63,6 +97,10 @@ public class CardTest extends TestCase {
         assertEquals("HP didn't go up", 20, healthCard.getHP());
     }
 
+    /**
+     * Tests the getAttack() method.
+     * @throws Exception
+     */
     @Test
     public void testGetAttack() throws Exception {
         assertNotNull("Attack cannot be null", card.getAttack());
