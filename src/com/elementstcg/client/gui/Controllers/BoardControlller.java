b388fad9b30ed92df13f27606177268abafeaf3a
Suigintou <dannyterhaar@msn.com>
2015-11-29 14:23:55 +0100

    New: Wrote the empty controller classes, some of the previous code has be rewritten, some of the previous code can be recycled, made the new classes to handle the migration to RMI more efficiently.

---
+++ src/com/elementstcg/client/gui/Controllers/BoardControlller.java
@@ -0,0 +1,42 @@
+public class BoardController {
+
+    public void GameOver(){
+
+    }
+    public void attackCard(Card card, int point){
+
+    }
+    public void putCardPlayer(Card card, int point){
+
+    }
+    public void updatePlayerHP(int hp){
+
+    }
+    public void removeCardPlayer(int point){
+
+    }
+    public void putCardEnemy(Card card, int point){
+
+    }
+    public void updateEnemyHp(int hp){
+
+    }
+    public void removeCardEnemy(int point){
+
+    }
+    public void updateUI(){
+
+    }
+    public void nextTurn(){
+
+    }
+    public void selectCard(CardPane cardPane){
+
+    }
+    public void showGhostPane(Node node){
+
+    }
+    public void hideGhostPane(){
+
+    }
+}
---
+++ src/com/elementstcg/client/gui/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+import java.lang.Boolean;
+
+public class LoginController{
+
+    public Account loginUser(String username, String password){
+        return null;
+    }
+
+    public Boolean registerUser(String username, String password, String email){
+
+        return false;
+    }
+}
