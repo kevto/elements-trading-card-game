2343a41e8177ee0d9134a1945812f518dd83d9c9
Suigintou <dannyterhaar@msn.com>
2015-11-29 14:23:55 +0100

    New: Wrote the empty controller classes, some of the previous code has be rewritten, some of the previous code can be recycled, made the new classes to handle the migration to RMI more efficiently.

---
+++ src/com/elementstcg/client/gui/Controllers/LobbyController.java
@@ -0,0 +1,11 @@
+public class LobbyController(){
+        public void findMatch(){
+
+        }
+        public void findMatchMaking(){
+
+        }
+        public void joinMatch(){
+
+        }
+        }
