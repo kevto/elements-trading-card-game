3588fda5c183e555df6d4486e02201b240ad4f48
Suigintou <dannyterhaar@msn.com>
2015-12-07 12:57:12 +0100

    Several small fixes

--- src/com/elementstcg/client/gui/CardPane.java
+++ src/com/elementstcg/client/gui/CardPane.java
@@ -1,6 +1,7 @@
 package com.elementstcg.client.gui;
 
 import com.elementstcg.client.Card;
+import com.elementstcg.client.gui.Controllers.BoardController;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
