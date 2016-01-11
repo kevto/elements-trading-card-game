e9fd1efca79bb94a7030782f32b9998f90b27c0d
kevto <berendsen.kevin@gmail.com>
2016-01-11 13:20:32 +0100

    Added timer that checks each 10 seconds if the players are still connected.

--- src/com/elementstcg/server/handlers/ServerHandler.java
+++ src/com/elementstcg/server/handlers/ServerHandler.java
@@ -58,6 +58,39 @@ public class ServerHandler extends UnicastRemoteObject implements IServerHandler
         games = new HashMap<>();
         searchingPlayers = new HashMap<>();
 
+        TimerTask checkAlive = new TimerTask() {
+            @Override
+            public void run() {
+                for(Map.Entry<String, Session> entry : clients.entrySet()) {
+                    if(entry.getValue().getClient() == null) {
+                        if(searchingPlayers.containsKey(entry.getKey())) {
+                            searchingPlayers.remove(entry.getKey());
+                        }
+
+                        // Removes the game if there's any.
+                        if(games.containsKey(entry.getValue().getBoardKey())) {
+                            Board game = games.get(entry.getValue().getBoardKey());
+
+                            Session otherPlayer = (!entry.getValue().equals(game.getPlayerOne().getSession()) ? game.getPlayerOne().getSession() : game.getPlayerTwo().getSession());
+                            if(otherPlayer.getClient() != null) {
+                                try {
+                                    otherPlayer.getClient().endMatch("Enemy forfeited!", true);
+                                } catch (RemoteException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            games.remove(game.getSessionKey());
+                        }
+
+                        clients.remove(entry.getKey());
+                    }
+                }
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(checkAlive, 10, 10 * 1000);
     }
 
     public IResponse login(IClientHandler client, String username, String password) throws RemoteException {
